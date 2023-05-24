0f5957ea7e721586b83b8705f644e1b08b507690
AlanZhangShuai <1484498126@qq.com>
2023-05-24 23:03:44 +0800

    @ 后序和中序列 构造 二叉树

--- swiftArithmetic/TreeNode.cpp
+++ swiftArithmetic/TreeNode.cpp
@@ -428,3 +428,42 @@ bool hasPathSumIteration(TreeNode* root, int sum) {
     }
     return false;
 }
+
+
+TreeNode* traversal (vector<int>& inorder, vector<int>& postorder) {
+    
+    if (postorder.size() == 0) {
+        return NULL;
+    }
+    int rootValue = postorder[postorder.size() -1];
+    TreeNode *root =  new TreeNode(rootValue);
+    if (postorder.size() == 1) {
+        return root;
+    }
+    int delimiterIndex;
+    for (delimiterIndex = 0; delimiterIndex < inorder.size() ; delimiterIndex ++) {
+        if (inorder[delimiterIndex] == rootValue) {
+            break;
+        }
+    }
+    
+    vector<int> leftInorder(inorder.begin(), inorder.begin() + delimiterIndex);
+    vector<int> rightInorder(inorder.begin() + delimiterIndex + 1, inorder.end());
+    
+    postorder.resize(postorder.size()-1);
+    
+    vector<int>leftPostorder(postorder.begin(), postorder.begin() + leftInorder.size());
+    
+    vector<int> rightPostorder(postorder.begin() + leftInorder.size(), postorder.end());
+    
+    root->left = traversal(leftInorder, leftPostorder);
+    root->right = traversal(rightInorder, rightPostorder);
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    if (inorder.size() ==0 || postorder.size() == 0) {
+        return NULL;
+    }
+    return traversal(inorder, postorder);
+}
