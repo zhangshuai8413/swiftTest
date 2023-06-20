12d2cb4255f0353bd0b7c146f63975be0f467b16
zhangshuai <zhangshuai@lippu.Itd>
2023-06-20 09:06:25 +0800

    加油站

--- swiftArithmetic/Greedy.cpp
+++ swiftArithmetic/Greedy.cpp
@@ -159,3 +159,24 @@ int jump(vector<int>& nums) {
     return ans;
 }
 
+
+
+int canCompleteCircuit(vector<int>&gas, vector<int>cost) {
+    int curSum = 0;
+    int totalSum = 0;
+    int start = 0;
+    for (int i = 0; i < gas.size() ;i ++) {
+        curSum += gas[i] - cost[i];
+        totalSum += gas[i] - cost[i];
+        if (curSum < 0) {
+            start  = i + 1;
+            curSum = 0;
+        }
+    }
+    if (totalSum < 0) {
+        return  -1;
+    }
+    return start;
+}
+
+
