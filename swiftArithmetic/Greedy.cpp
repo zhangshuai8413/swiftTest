71ad92119c6a5f640e88ccbc5fa11b42208a8e81
AlanZhangShuai <1484498126@qq.com>
2023-06-27 07:40:39 +0800

    @feat  划分字母

--- swiftArithmetic/Greedy.cpp
+++ swiftArithmetic/Greedy.cpp
@@ -357,4 +357,41 @@ public:
         return result;
 
     }
+    
+};
+
+class PartitionLabels {
+   /*
+    字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+
+    示例：
+
+    输入：S = "ababcbacadefegdehijhklij"
+    输出：[9,7,8] 解释： 划分结果为 "ababcbaca", "defegde", "hijhklij"。 每个字母最多出现在一个片段中。 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+    提示：
+
+    S的长度在[1, 500]之间。
+    S只包含小写字母 'a' 到 'z' 。
+    */
+public:
+    
+    vector<int>partitionLabels(string S) {
+        int hash[27] = {0};
+        vector<int>result;
+        int right = 0;
+        int left = 0;
+        for (int i=0; i< S.size(); ++i) {
+            hash[S[i] - 'a'] = i;
+        }
+
+        for (int i=0; i< S.size(); ++i) {
+            right = max(right, hash[S[i] - 'a']);
+            if (right == i) {
+                result.push_back(right - left + 1);
+                left = i + 1;
+            }
+        }
+        return result;
+    }
+    
 };
