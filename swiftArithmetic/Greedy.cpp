3ea7750f2c3f34f83f818b7fa32a378114e943a8
zhangshuai <zhangshuai@lippu.Itd>
2023-06-27 08:22:28 +0800

    @feat  合并区间

--- swiftArithmetic/Greedy.cpp
+++ swiftArithmetic/Greedy.cpp
@@ -395,3 +395,52 @@ public:
     }
     
 };
+
+class EraseOverlapIntervals {
+    
+    /*
+     给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+
+     示例 1:
+
+     输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+     输出: 1
+     解释: 移除 [1,3] 后，剩下的区间没有重叠。
+     示例 2:
+
+     输入: intervals = [ [1,2], [1,2], [1,2] ]
+     输出: 2
+     解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+     示例 3:
+
+     输入: intervals = [ [1,2], [2,3] ]
+     输出: 0
+     解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+     */
+    
+public:
+    
+    static bool cmp(const vector<int> &a, const vector<int> &b) {
+        return  a[0]< b[0];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0) {
+            return 0;
+        }
+        sort(intervals.begin(), intervals.end(), cmp);
+        int count = 0;
+        int end = intervals[0][1];
+        for (int i = 1; i <intervals.size(); i ++) {
+            if(intervals[i][0] >= end) {
+                end =  intervals[i][1];
+            } else {
+               count ++;
+               end = min(intervals[i][1], end);
+            }
+            
+        }
+        
+        return count;
+    }
+    
+};
