d83e00098cb1681407bb71ad82a0b37d67626e41
zhangshuai <zhangshuai@lippu.Itd>
2023-06-29 09:36:25 +0800

    @feat  不同路径

--- swiftArithmetic/DynamicProgram.hpp
+++ swiftArithmetic/DynamicProgram.hpp
@@ -66,7 +66,6 @@ public:
         }
         return dp[1];
     }
-    
 };
 
 
@@ -156,6 +155,27 @@ public:
         }
         for (int i = 1; i <m; i ++) {
             for (int j = 1; j <n; j ++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = (int)obstacleGrid.size();
+        int n = (int)obstacleGrid[0].size();
+        vector<vector<int>>dp(m, vector<int>(n,0));
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        }
+        for (int i = 0; i <m && obstacleGrid[i][0] == 0; i ++)dp[i][0] = 1;
+        for (int i = 0; i <n && obstacleGrid[0][i] == 0; i ++)dp[0][i] = 1;
+        for (int i = 1; i <m; i ++) {
+            for (int j = 1; j <n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
