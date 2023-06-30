58d20479871dd1d0eb8c5be7db745b23fcb907bd
zhangshuai <zhangshuai@lippu.Itd>
2023-06-30 09:17:26 +0800

    @feat  整数划分

--- swiftArithmetic/DynamicProgram.hpp
+++ swiftArithmetic/DynamicProgram.hpp
@@ -182,4 +182,54 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+
+
+class InterBreake {
+    /*
+     给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+     示例 1:
+     输入: 2
+     输出: 1
+     解释: 2 = 1 + 1, 1 × 1 = 1。
+     示例 2:
+     
+     输入: 10
+     输出: 36
+     解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+     说明: 你可以假设 n 不小于 2 且不大于 58。
+     */
+    
+public:
+    int integerBreak(int n) {
+        vector<int>dp(n+1);
+        dp[2] = 1;
+        for (int i = 3; i <=n; i ++) {
+            for (int j = 1; j <= i / 2; j ++) {
+                dp[i] = max(dp[i], max((i -j) * j, dp[i-j] *j));
+            }
+        }
+        return dp[n];
+    }
+    
+    
+    int integerBreakII(int n) {
+        
+        if(n == 2) {
+            return 1;
+        } else if(n == 3) {
+            return 2;
+        }  else if(n == 4) {
+            return 4;
+        }
+        int result = 1;
+        while (n > 4) {
+            result *= 3;
+            n-=3;
+        }
+        result *= n;
+        return result;
+        
+    }
+};
 #endif /* DynamicProgram_hpp */
