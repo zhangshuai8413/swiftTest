d00d752613fe057a89ebdda67d4d987cbb7280ae
zhangshuai <zhangshuai@lippu.Itd>
2023-07-10 08:46:52 +0800

    @feat 0 和1

--- swiftArithmetic/DynamicProgram.hpp
+++ swiftArithmetic/DynamicProgram.hpp
@@ -424,3 +424,54 @@ public:
     }
     
 };
+
+
+class FindTargetSumWays {
+    /*
+     给你一个整数数组 nums 和一个整数 target 。
+
+     向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+
+     例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+     返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+
+      
+
+     示例 1：
+
+     输入：nums = [1,1,1,1,1], target = 3
+     输出：5
+     解释：一共有 5 种方法让最终目标和为 3 。
+     -1 + 1 + 1 + 1 + 1 = 3
+     +1 - 1 + 1 + 1 + 1 = 3
+     +1 + 1 - 1 + 1 + 1 = 3
+     +1 + 1 + 1 - 1 + 1 = 3
+     +1 + 1 + 1 + 1 - 1 = 3
+     示例 2：
+
+     输入：nums = [1], target = 1
+     输出：1
+      
+     */
+    
+public:
+    
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for (int i = 0; i <nums.size(); i ++) sum += nums[i];
+        if (abs(S) > sum || (S + sum) %2 == 1) {
+            return 0;
+        }
+        int bagSize = (S + sum) / 2;
+        vector<int>dp(bagSize + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <nums.size(); i ++) {
+            for (int j =  bagSize; j >=nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
+        
+    }
+    
+};
