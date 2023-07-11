fd3b9c566a494b6985c985003df6e46a57384243
zhangshuai <zhangshuai@lippu.Itd>
2023-07-11 08:54:05 +0800

    @feat 零钱兑换

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -493,3 +493,52 @@ public:
     }
     
 };
+
+
+class ChangeCoinII {
+    
+    /*
+     518. 零钱兑换 II
+     给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 aamount 表示总金额。
+
+     请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+
+     假设每一种面额的硬币有无限个。
+
+     题目数据保证结果符合 32 位带符号整数。
+
+      
+
+     示例 1：
+
+     输入：aamount = 5, coins = [1, 2, 5]
+     输出：4
+     解释：有四种方式可以凑成总金额：
+     5=5
+     5=2+2+1
+     5=2+1+1+1
+     5=1+1+1+1+1
+     示例 2：
+
+     输入：aamount = 3, coins = [2]
+     输出：0
+     解释：只用面额 2 的硬币不能凑成总金额 3 。
+     示例 3：
+
+     输入：aamount = 10, coins = [10]
+     输出：1
+     */
+public:
+    
+    int change(int amount, vector<int>&coins) {
+        vector<int>dp(amount+1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <coins.size(); i ++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
+                
+            }
+        }
+        return dp[amount];
+    }
+};
