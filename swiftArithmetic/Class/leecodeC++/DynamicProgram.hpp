8486c70a69139fb02d351f2c49fee116f7c64a84
zhangshuai <zhangshuai@lippu.Itd>
2023-07-12 09:47:26 +0800

    @feat 组合和  排列组合

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -542,3 +542,39 @@ public:
         return dp[amount];
     }
 };
+
+
+class CombinationSum4 {
+    /*
+     难度：中等
+
+     给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
+
+     示例:
+
+     nums = [1, 2, 3]
+     target = 4
+     所有可能的组合为： (1, 1, 1, 1) (1, 1, 2) (1, 2, 1) (1, 3) (2, 1, 1) (2, 2) (3, 1)
+
+     请注意，顺序不同的序列被视作不同的组合。
+
+     因此输出为 7。
+     */
+    
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+ 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <= target; i ++) {
+            for (int j = 0; j <nums.size(); j ++) {
+                
+                if (i - nums[j] >=0 &&  dp[i] < INT_MAX - dp[i - nums[j]]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return  dp[target];
+        
+    }
+    
+};
