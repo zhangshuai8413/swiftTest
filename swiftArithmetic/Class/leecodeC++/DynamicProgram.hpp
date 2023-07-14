b09f2f700d2a7b05a7f4b35794008a10b3b3bca3
zhangshuai <zhangshuai@lippu.Itd>
2023-07-14 09:51:22 +0800

    @fix 零钱兑换

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -541,6 +541,22 @@ public:
         }
         return dp[amount];
     }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i <coins.size(); i ++) {
+            for (int j =  coins[i]; j <= amount; j++) {
+                if(dp[j - coins[i]] != INT_MAX ) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        if (dp[amount] == INT_MAX) {
+            return  -1;
+        }
+        return dp[amount];
+    }
 };
 
 
