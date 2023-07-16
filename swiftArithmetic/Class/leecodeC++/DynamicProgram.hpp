1a3e4ffcf0535ae79b1b346d50404cedb4a5a60b
AlanZhangShuai <1484498126@qq.com>
2023-07-16 11:32:14 +0800

    @fix  打家劫舍

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -728,3 +728,70 @@ public:
         return dp[nums.size() - 1];
     }
 };
+
+class RobII {
+    /*
+     力扣题目链接
+
+     你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+
+     给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。
+
+     示例 1：
+
+     输入：nums = [2,3,2]
+
+     输出：3
+
+     解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+
+     示例 2：
+
+     输入：nums = [1,2,3,1]
+
+     输出：4
+
+     解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。偷窃到的最高金额 = 1 + 3 = 4 。
+
+     示例 3：
+
+     输入：nums = [0]
+
+     输出：0
+
+     提示：
+
+     1 <= nums.length <= 100
+     0 <= nums[i] <= 1000
+     */
+    
+public:
+    
+    int robRange(vector<int>& nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        }
+        vector<int>dp(nums.size());
+        dp[start] = nums[start];
+        dp[start + 1] = max(nums[start], nums[start + 1]);
+        for (int i= start + 2; i<= end; ++i) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+            
+        }
+        return dp[end];
+    }
+    
+    int rob(vector<int> &nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        int result1 = robRange(nums, 0, (int)nums.size() -2);
+        int result2 = robRange(nums, 1, (int)nums.size() -1);
+        return max(result1, result2);
+    }
+    
+  
+};
