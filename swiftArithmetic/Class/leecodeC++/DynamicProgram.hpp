9140d7e1507ae2749c78857aaaeb89ff3dc283c4
AlanZhangShuai <1484498126@qq.com>
2023-07-22 21:44:57 +0800

    @fix  最长子串

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -877,4 +877,21 @@ public:
         if (dp[s.size()][t.size()] == s.size()) return true;
         return false;
     }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return (int)nums.size();
+        }
+        vector<int>dp(nums.size(), 1);
+        int result = 0;
+        for (int i = 0; i< nums.size(); ++i) {
+            for (int j = 0; j< i; ++j) {
+                if (nums[j] > nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+                result = max(result, dp[i]);
+            }
+        }
+        return result;
+    }
 };
