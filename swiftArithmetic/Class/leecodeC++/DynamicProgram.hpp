67d1da0f648952223c9beccdc0ab4e56769c99e8
AlanZhangShuai <1484498126@qq.com>
2023-07-24 22:12:19 +0800

    @fix 最大子数组和

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -1043,10 +1043,31 @@ public:
                 } else {
                     dp[i][j] = max(dp[i -1][j], dp[i][j -1]);
                 }
-                
             }
-
         }
         return dp[nums1.size()][nums2.size()];
+    }
+    
+    
+    
+    /*
+     给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+     示例:
+
+     输入: [-2,1,-3,4,-1,2,1,-5,4]
+     输出: 6
+     解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     */
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        vector<int>dp(nums.size()+ 1);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i=1; i< nums.size(); ++i) {
+            dp[i] = max(nums[i], dp[i-1] + nums[i]);
+            result = max(result, dp[i]);
+        }
+        return result;
     }
 };
