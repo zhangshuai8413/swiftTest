c7292c44a16787be38d4f435d1c88c4982b341a8
AlanZhangShuai <1484498126@qq.com>
2023-07-24 22:31:31 +0800

    @fix  删除 字符串

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -1128,3 +1128,44 @@ public:
         return result;
     }
 };
+
+class MinDistance {
+    /*
+     583. 两个字符串的删除操作
+     力扣题目链接
+
+     给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+
+     示例：
+
+     输入: "sea", "eat"
+     输出: 2
+     解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+     */
+    
+    
+     /*
+      注意点
+        开始下表 
+      */
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size() +1));
+        for (int i = 0; i <= word1.size(); i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= word2.size(); j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j ++) {
+                if(word1[i -1] == word2[j -1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+};
