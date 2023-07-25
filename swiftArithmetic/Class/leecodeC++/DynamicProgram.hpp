2a1cfaece87e5a64bff6cf97826968de4283748c
AlanZhangShuai <1484498126@qq.com>
2023-07-25 22:56:09 +0800

    @fix 最长回文字符串

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -1267,3 +1267,37 @@ public:
         return result;
     }
 };
+
+
+class LongestPalindromeSubseq {
+    /*
+     给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+
+     示例 1: 输入: "bbbab" 输出: 4 一个可能的最长回文子序列为 "bbbb"。
+
+     示例 2: 输入:"cbbd" 输出: 2 一个可能的最长回文子序列为 "bb"。
+
+     提示：
+
+
+     
+     */
+    
+public:
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>>dp(s.size(),vector<int>(s.size(),0));
+        for (int i = 0; i < s.size() ; i ++) {
+            dp[i][i] = 1;
+        }
+        for (int i = (int)s.size() - 1; i >= 0 ; i--) {
+            for (int j = i + 1; j < s.size() ; j ++) {
+                if(s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j -1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j -1]);
+                }
+            }
+        }
+        return dp[0][s.size() -1];
+    }
+};
