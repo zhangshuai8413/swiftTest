be4e9a42b65228e0fb370c533a59972a15e3d67c
zhangshuai <zhangshuai@lippu.Itd>
2023-07-25 09:12:57 +0800

    @feat  最长回文子串

--- swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
+++ swiftArithmetic/Class/leecodeC++/DynamicProgram.hpp
@@ -1227,3 +1227,43 @@ public:
         return dp[word1.size()][word2.size()];
     }
 };
+
+
+class CountSubstrings {
+    /*
+     647. 回文子串
+     力扣题目链接
+
+     给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+
+     具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+     示例 1：
+
+     输入："abc"
+     输出：3
+     解释：三个回文子串: "a", "b", "c"
+     示例 2：
+
+     输入："aaa"
+     输出：6
+     解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+     提示：输入的字符串长度不会超过 1000 。
+     */
+    
+public:
+    int countSubstrings(string s) {
+        if(s.size() <= 1) return (int)s.size();
+        vector<vector<bool>>dp(s.size(), vector<bool>(s.size(),false));
+        int result = 0;
+        for (int i = (int)s.size(); i >=0 ; i--) {
+            for (int j = i; j <s.size(); j ++) {
+                if(s[i] == s[j] && ((j -i) <= 1 || dp[i+1][j -1])) {
+                    result += 1;
+                    dp[i][j]= true;
+                }
+            }
+        }
+        return result;
+    }
+};
