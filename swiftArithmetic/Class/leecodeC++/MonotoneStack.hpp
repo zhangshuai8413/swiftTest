afcee3074699158d1d9f5b430845a94d35c22801
zhangshuai <zhangshuai@lippu.Itd>
2023-07-27 09:20:58 +0800

    @fix 接雨点  单调栈

--- swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
+++ swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
@@ -70,4 +70,41 @@ public:
         return result;
     }
 };
+
+class Trap {
+    /*
+     给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+     示例 1：
+     输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+     输出：6
+     解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+     示例 2：
+
+     输入：height = [4,2,0,3,2,5]
+     输出：9
+
+     */
+    
+public:
+    
+    int trap(vector<int>& height) {
+        stack<int>st;
+        st.push(0);
+        int sum = 0;
+        for (int i = 0; i <height.size(); i ++) {
+            while (!st.empty() && height[i] > st.top()) {
+                int mid = st.top();
+                st.pop();
+                if(!st.empty()) {
+                    int h = min(height[st.top()], height[i] - height[mid]);
+                    int w = i - st.top() - 1;
+                    sum += h * w;
+                }
+            }
+            st.push(i);
+        }
+        return sum;
+    }
+};
 #endif /* MonotoneStack_hpp */
