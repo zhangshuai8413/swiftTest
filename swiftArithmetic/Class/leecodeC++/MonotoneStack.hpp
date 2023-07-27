2a838ca8076d1865701b3cfa54bebfaacbdf9251
zhangshuai <zhangshuai@lippu.Itd>
2023-07-27 08:52:49 +0800

    @feat 下个更大元素

--- swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
+++ swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
@@ -37,4 +37,37 @@ public:
         return result;
     }
 };
+
+class NextGreatNum {
+    /*
+     力扣题目链接
+
+     给定一个循环数组（最后一个元素的下一个元素是数组的第一个元素），输出每个元素的下一个更大元素。数字 x 的下一个更大的元素是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1。
+
+     示例 1:
+
+     输入: [1,2,1]
+     输出: [2,-1,2]
+     解释: 第一个 1 的下一个更大的数是 2；数字 2 找不到下一个更大的数；第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+     提示:
+
+     1 <= nums.length <= 10^4
+     -10^9 <= nums[i] <= 10^9
+     */
+    
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int>result(nums.size(), -1);
+        stack<int>st;
+        st.push(0);
+        for (int i = 0; i <nums.size() * 2; i ++) {
+            while (!st.empty() && nums[i % nums.size()]> nums[st.top()]) {
+                result[st.top()] = nums[i % nums.size()];
+                st.pop();
+            }
+            st.push(i % nums.size());
+        }
+        return result;
+    }
+};
 #endif /* MonotoneStack_hpp */
