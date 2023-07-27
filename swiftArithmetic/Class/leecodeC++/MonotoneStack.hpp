0b89bba5810201c7eb542a9002c586e642f97707
zhangshuai <zhangshuai@lippu.Itd>
2023-07-27 09:43:14 +0800

    @fix 接雨点 动态规划

--- swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
+++ swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
@@ -106,5 +106,28 @@ public:
         }
         return sum;
     }
+    
+    int trapII(vector<int>& height) {
+        if(height.size() < 2) return 0;
+        vector<int>maxLeft(height.size(), 0);
+        vector<int>maxRight(height.size(), 0);
+        int size = (int)height.size();
+        maxLeft[0] = 0;
+        for (int i = 1; i <size; i ++) {
+            maxLeft[i] = max(height[i], maxLeft[i-1]);
+        }
+        maxRight[size -1] = height[size -1];
+        for (int i = size - 2; i >= 0; i--) {
+            maxRight[i] = max(height[i], maxRight[i +1]);
+        }
+        int sum = 0;
+        for (int i = 0; i <size; i ++) {
+            int count = min(maxLeft[i], maxRight[i] - height[i]);
+            sum += count;
+        }
+        return sum;
+    }
 };
+
+
 #endif /* MonotoneStack_hpp */
