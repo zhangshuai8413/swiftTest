3a9056cb471a74b916ed64de8cfada435624b8ca
zhangshuai <zhangshuai@lippu.Itd>
2023-08-03 09:27:13 +0800

    @fix  被围区域

--- swiftArithmetic/Class/leecodeC++/Graph.hpp
+++ swiftArithmetic/Class/leecodeC++/Graph.hpp
@@ -163,4 +163,117 @@ public:
         return result;
     }
 };
+
+
+class MumEnclaves {
+    /*
+     
+     1020. 飞地的数量
+     给你一个大小为 m x n 的二进制矩阵 grid ，其中 0 表示一个海洋单元格、1 表示一个陆地单元格。
+
+     一次 移动 是指从一个陆地单元格走到另一个相邻（上、下、左、右）的陆地单元格或跨过 grid 的边界。
+
+     返回网格中 无法 在任意次数的移动中离开网格边界的陆地单元格的数量。
+     */
+    
+public:
+    int count;
+    int dir[4][2] =  {-1,0, 0, -1, 1,0, 0, 1};
+    
+    void dfs(vector<vector<int>>& grid, int x, int y) {
+        grid[x][y] = 0;
+        count ++;
+        for (int i = 0; i <4; i ++) {
+            int nextX = x + dir[i][0];
+            int nextY = y + dir[i][1];
+            if (nextX < 0 ||  nextX >= grid.size() ||  nextY < 0 ||  nextY >= grid[0].size()) {
+                continue;
+            }
+            if (grid[nextX][nextY] == 0) continue;
+            
+            dfs(grid, nextX, nextY);
+                
+        }
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = (int)grid.size(), m = (int)grid[0].size();
+        for (int i = 0; i <n; i ++) {
+            if (grid[i][0] == 1) dfs(grid, i, 0);
+            if (grid[i][ m - 1] == 1) {
+                dfs(grid, i, m -1);
+            }
+        }
+        
+        for (int j = 0; j <grid[0].size(); j ++) {
+            if (grid[0][j] == 1) dfs(grid, 0, j);
+            if(grid[n -1][j] == 1) dfs(grid, n -1, j);
+        }
+        count = 0;
+        for (int i = 0; i <n; i ++) {
+            for (int j = 0; j <m; j ++) {
+                if(grid[i][j] == 1) dfs(grid, i, j);
+            }
+        }
+        return count;
+        
+    }
+    
+};
+
+class Solve {
+    /*
+     参与本项目，贡献其他语言版本的代码，拥抱开源，让更多学习算法的小伙伴们收益！
+
+     130. 被围绕的区域
+     题目链接
+
+     给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+
+
+
+     输入：board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+     输出：[["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+     解释：被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+
+     */
+    
+public:
+    int dir[4][2] = {-1, 0, 0, -1, 1, 0, 0, 1}; // 保存四个方向
+    void dfs(vector<vector<char>>& board, int x, int y) {
+        board[x][y] = 'A';
+        for (int i = 0; i < 4; i++) { // 向四个方向遍历
+            int nextx = x + dir[i][0];
+            int nexty = y + dir[i][1];
+            // 超过边界
+            if (nextx < 0 || nextx >= board.size() || nexty < 0 || nexty >= board[0].size()) continue;
+            // 不符合条件，不继续遍历
+            if (board[nextx][nexty] == 'X' || board[nextx][nexty] == 'A') continue;
+            dfs (board, nextx, nexty);
+        }
+    }
+    
+    void solve(vector<vector<char>>& board) {
+          int n = board.size(), m = board[0].size();
+          // 步骤一：
+          // 从左侧边，和右侧边 向中间遍历
+          for (int i = 0; i < n; i++) {
+              if (board[i][0] == 'O') dfs(board, i, 0);
+              if (board[i][m - 1] == 'O') dfs(board, i, m - 1);
+          }
+
+          // 从上边和下边 向中间遍历
+          for (int j = 0; j < m; j++) {
+              if (board[0][j] == 'O') dfs(board, 0, j);
+              if (board[n - 1][j] == 'O') dfs(board, n - 1, j);
+          }
+          // 步骤二：
+          for (int i = 0; i < n; i++) {
+              for (int j = 0; j < m; j++) {
+                  if (board[i][j] == 'O') board[i][j] = 'X';
+                  if (board[i][j] == 'A') board[i][j] = 'O';
+              }
+          }
+      }
+};
 #endif /* Graph_hpp */
