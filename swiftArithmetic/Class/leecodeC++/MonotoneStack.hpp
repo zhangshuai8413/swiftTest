8ae57286846b1e32b92d1c2d9d0ed1814634dd68
AlanZhangShuai <1484498126@qq.com>
2023-08-19 20:57:06 +0800

    @fix  最长有效字符串

--- swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
+++ swiftArithmetic/Class/leecodeC++/MonotoneStack.hpp
@@ -128,5 +128,45 @@ public:
     }
 };
 
+/*
+ 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
 
+  
+
+ 示例 1：
+
+ 输入：s = "(()"
+ 输出：2
+ 解释：最长有效括号子串是 "()"
+ 示例 2：
+
+ 输入：s = ")()())"
+ 输出：4
+ 解释：最长有效括号子串是 "()()"
+ 示例 3：
+
+ 输入：s = ""
+ 输出：0
+ */
+
+int longestValidParentheses(string s) {
+    
+    int maxans = 0;
+    stack<int> stk;
+    stk.push(-1);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            stk.push(i);
+        } else {
+            stk.pop();
+            if (stk.empty()) {
+                stk.push(i);
+            } else {
+                maxans = max(maxans, i - stk.top());
+            }
+        }
+    }
+    return maxans;
+    
+}
 #endif /* MonotoneStack_hpp */
