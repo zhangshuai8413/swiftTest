85d2d6d48db7b111e48499604d7088cf3edb85c2
AlanZhangShuai <1484498126@qq.com>
2023-08-19 15:54:20 +0800

    @fix  最大区域

--- swiftArithmetic/Class/leecodeC++/MyArray.hpp
+++ swiftArithmetic/Class/leecodeC++/MyArray.hpp
@@ -301,63 +301,88 @@ public:
 class searchII {
     /*
      整数数组 nums 按升序排列，数组中的值 互不相同 。
-
+     
      在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
-
+     
      给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
-
+     
      你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
-
-      
-
+     
+     
+     
      示例 1：
-
+     
      输入：nums = [4,5,6,7,0,1,2], target = 0
      输出：4
      示例 2：
-
+     
      输入：nums = [4,5,6,7,0,1,2], target = 3
      输出：-1
      示例 3：
-
+     
      输入：nums = [1], target = 0
      输出：-1
-      
+     
      */
     
 public:
     
     int search(vector<int>& nums, int target) {
-            int n = (int)nums.size();
-            if (!n) {
-                return -1;
-            }
-            if (n == 1) {
-                return nums[0] == target ? 0 : -1;
-            }
-            int l = 0, r = n - 1;
-            while (l <= r) {
-                int mid = (l + r) / 2;
-                if (nums[mid] == target) return mid;
-                if (nums[0] <= nums[mid]) {
-                    if (nums[0] <= target && target < nums[mid]) {
-                        r = mid - 1;
-                    } else {
-                        l = mid + 1;
-                    }
-                } else {
-                    if (nums[mid] < target && target <= nums[n - 1]) {
-                        l = mid + 1;
-                    } else {
-                        r = mid - 1;
-                    }
-                }
-            }
+        int n = (int)nums.size();
+        if (!n) {
             return -1;
         }
-
-    
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 };
 
+class MaxArea {
+    /*
+     给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+
+     找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+     返回容器可以储存的最大水量。
+
+     说明：你不能倾斜容器。
+     */
+    
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = (int)height.size() - 1;
+        int ans = 0;
+        while (l <= r) {
+            int are = min(height[l], height[r]) * (r - l);
+            ans = max(ans, are);
+            if (height[l]<= height[r]) {
+                ++l;
+            }else {
+                --r;
+            }
+        }
+        return ans;
+    }
+};
 
 #endif /* MyArray_hpp */
