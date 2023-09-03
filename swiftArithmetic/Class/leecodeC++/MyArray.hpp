9b9da6ef70b65ceda83c6c644a5b612b101a7e4a
AlanZhangShuai <1484498126@qq.com>
2023-09-03 20:08:40 +0800

    @fix  三角形最小路径

--- swiftArithmetic/Class/leecodeC++/MyArray.hpp
+++ swiftArithmetic/Class/leecodeC++/MyArray.hpp
@@ -434,5 +434,35 @@ public:
 };
 
 
+class PlusOne {
+    
+public:
+    /*
+     
+     作者：力扣官方题解
+     链接：https://leetcode.cn/problems/plus-one/solutions/1057162/jia-yi-by-leetcode-solution-2hor/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    vector<int> plusOne(vector<int>& digits) {
+        int n = (int)digits.size();
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < n; ++j) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+        
+        // digits 中所有的元素均为 9
+        vector<int> ans(n + 1);
+        ans[0] = 1;
+        return ans;
+    }
+};
+
+
 
 #endif /* MyArray_hpp */
