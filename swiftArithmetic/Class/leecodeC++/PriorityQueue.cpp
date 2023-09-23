f2460f372e32f4c454a10870a4ea51d6d4934d8d
AlanZhangShuai <1484498126@qq.com>
2023-09-23 13:33:29 +0800

    @feat 优先队列题目

---
+++ swiftArithmetic/Class/leecodeC++/PriorityQueue.cpp
@@ -0,0 +1,8 @@
+//
+//  PriorityQueue.cpp
+//  swiftArithmetic
+//
+//  Created by 张帅 on 2023/9/23.
+//
+
+#include "PriorityQueue.hpp"
---
+++ swiftArithmetic/Class/leecodeC++/PriorityQueue.hpp
@@ -0,0 +1,13 @@
+//
+//  PriorityQueue.hpp
+//  swiftArithmetic
+//
+//  Created by 张帅 on 2023/9/23.
+//
+
+#ifndef PriorityQueue_hpp
+#define PriorityQueue_hpp
+#include "CPlusHeader.h"
+#include <stdio.h>
+
+#endif /* PriorityQueue_hpp */
