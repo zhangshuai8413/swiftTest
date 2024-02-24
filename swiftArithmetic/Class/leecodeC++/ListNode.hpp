2dbd2e02478dc17ef9aeb514df7000b4c1cfc5ac
“zhangshuai” <zhangshuai.90@bytedance.com>
2024-02-24 18:29:50 +0800

    @fix 翻转链表

--- swiftArithmetic/Class/leecodeC++/ListNode.hpp
+++ swiftArithmetic/Class/leecodeC++/ListNode.hpp
@@ -519,3 +519,26 @@ public:
     }
 };
 
+class ReverseList {
+    
+    
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode *dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        ListNode *pre = dummyNode;
+        for (int i = 0; i< left - 1 ; i ++) {
+            pre = pre->next;
+        }
+        ListNode *cur = pre->next;
+        ListNode *next;
+        for (int i = 0; i< right - left; i ++) {
+            next = cur->next;
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+            
+        }
+        return dummyNode->next;
+    }
+};
