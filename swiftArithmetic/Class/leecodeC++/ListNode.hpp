8ff1d03c25b1212dff29c005490a949d3db50833
AlanZhangShuai <1484498126@qq.com>
2024-06-16 11:11:19 +0800

    @feat 搜索二叉树数量

--- swiftArithmetic/Class/leecodeC++/ListNode.hpp
+++ swiftArithmetic/Class/leecodeC++/ListNode.hpp
@@ -577,3 +577,28 @@ public:
     }
 };
 
+class NumTrees {
+    // 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+    
+    /*
+    示例 1：
+    输入：n = 3
+    输出：5
+    示例 2：
+
+    输入：n = 1
+    输出：1
+*/
+    int numTrees(int n) {
+        std::vector<int> dp(n + 1, 0);
+        dp[0] = 1;  // 空树的数量为1
+        dp[1] = 1;  // 只有一个节点的树的数量为1
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
