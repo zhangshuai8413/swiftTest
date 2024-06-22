6d5518e647e85c3ca317cf81c427024967ea0388
AlanZhangShuai <1484498126@qq.com>
2024-06-22 21:44:21 +0800

    @feat 链表排序

--- swiftArithmetic/Class/leecodeC++/ListNode.hpp
+++ swiftArithmetic/Class/leecodeC++/ListNode.hpp
@@ -602,3 +602,56 @@ class NumTrees {
         return dp[n];
     }
 };
+
+
+class SortList {
+    
+public:
+    
+    ListNode* getMiddle(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val < temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        
+        return dummyHead->next;
+    }
+  
+    ListNode* sortList(ListNode* head) {
+   
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode *mid = getMiddle(head);
+        ListNode *right = mid->next;
+        mid->next = nullptr;
+        
+        ListNode * leftSort = sortList(head);
+        ListNode * rightSort = sortList(right);
+        return merge(leftSort, rightSort);
+        
+    }
+};
