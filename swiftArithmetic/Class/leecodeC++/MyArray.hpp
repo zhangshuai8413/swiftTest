183a7afa674f81db61d06d04e24509d3a6630b9d
AlanZhangShuai <1484498126@qq.com>
2025-03-12 00:43:21 +0800

    revert  int

--- swiftArithmetic/Class/leecodeC++/MyArray.hpp
+++ swiftArithmetic/Class/leecodeC++/MyArray.hpp
@@ -796,6 +796,18 @@ class RemoveElement {
         }
         return left;
     }
-
+    
+    int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            if (ret > INT_MAX / 10 || ret < INT_MIN / 10) {
+                return 0;
+            } else {
+                ret = ret + x % 10;
+                x = x / 10;
+            }
+        }
+        return ret;
+    }
 };
 #endif /* MyArray_hpp */
