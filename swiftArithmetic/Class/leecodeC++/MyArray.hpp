3bb0e756fec65b5293fd42e287e409821b88d6a8
AlanZhangShuai <1484498126@qq.com>
2025-03-12 21:24:46 +0800

    fix  有序数组的平方

--- swiftArithmetic/Class/leecodeC++/MyArray.hpp
+++ swiftArithmetic/Class/leecodeC++/MyArray.hpp
@@ -809,5 +809,24 @@ class RemoveElement {
         }
         return ret;
     }
+    
+    vector<int> sortedSquares(vector<int>& nums) {
+          int size = (int)nums.size();
+          int k = size - 1;
+          vector<int>result(size,0);
+          int i = 0;
+          int j = k;
+          while (i <= j) {
+              if (nums[i] * nums[i] < nums[j] *nums[j]) {
+                  result[k--] = nums[j] * nums[j];
+                  j --;
+              } else {
+                  result[k--] = nums[i] * nums[i];
+                  i++;
+              }
+          }
+          return result;
+      }
+    
 };
 #endif /* MyArray_hpp */
